38ca2becaba60b8b763c1fcef982acad4205029a
Valshiran <leftythedrummer88@gmail.com>
2026-08-15 12:19:09 -0600

    Updated model.py to add the code for the functions listed in Step 3

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,12 @@
+from os import path
 import pickle
+from xml.parsers.expat import model
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 from ml.data import process_data
-# TODO: add necessary import
+import os
+import joblib
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,8 +24,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +65,10 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    
+    # Generate predictions using fitted estimator
+    return model.predict(X)
+
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +80,19 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    # checks to see if the directory exists, if not it creates it
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    
+    # uses dump to dump the model to a file
+    joblib.dump(model, path)
+    print(f"Model saved to {path}")
+
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    
+    print(f"Loading model from {path}")
+    return joblib.load(path)
 
 
 def performance_on_categorical_slice(
@@ -117,12 +131,21 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    
+    # filters the df to target feature
+    slice_df = data[data[column_name] == slice_value]
+    
+    # pricess slice data using fitted transformers
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+       slice_df,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    
+    # run inference function and compute metrics on the slice
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
